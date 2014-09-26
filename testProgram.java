a0e31f49e0b3c3d2692465a8ba6bb410e6b3585f
Sam Bituser <admin@bituser.com>
2014-09-26 15:02:37 +1200

    Added a useless commit to the useless program :D

--- testProgram.java
+++ testProgram.java
@@ -1,5 +1,8 @@
 public class testProgram{
 
+    /**
+     * This program is useless
+     */
     public static void main(String[] args){
         System.out.println("I'm a completely useless program!. I'm not even sure I compile :(");
     }
