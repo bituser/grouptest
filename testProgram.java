db7e2b396d59ca5ab72e55cad913a299594b5fc7
Phil <Phil@phil.com>
2014-09-28 12:27:06 +1300

    Another change

--- testProgram.java
+++ testProgram.java
@@ -6,7 +6,7 @@ public class testProgram{
     public static void main(String[] args){
     	System.out.println("I'm a completely useless program!. I'm not even sure I compile :(");
 		System.out.println("Don't worry it compiles");
-		System.out.println("What is your name?");
+		System.out.println("What is your name??");
 		String name = "";
 		System.out.println("Hello, your name is " + name);
     	}
