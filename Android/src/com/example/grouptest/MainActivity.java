ad479f9c57333390a4762dc56b400ea60a4938ac
Phil <Phil@phil.com>
2014-10-05 15:15:30 +1300

    Added some random changes. Only want 1.

--- Android/src/com/example/grouptest/MainActivity.java
+++ Android/src/com/example/grouptest/MainActivity.java
@@ -1,21 +1,81 @@
 package com.example.grouptest;
 
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract.Events;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+ 
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+ 
+            @Override
+            public void onClick(View arg0) {
+ 
+                //add event to calendar
+                addCalendarCourseAndEvent();
+            	//addCalendarEvent();
+            }
+ 
+        });
+        
+}
+    
+    protected void addCalendarCourseAndEvent() {
+		// TODO Auto-generated method stub
+    	Calendar startDateCal = new GregorianCalendar();
+    	startDateCal.set(Calendar.YEAR, 2014);
+    	startDateCal.set(Calendar.MONTH, Calendar.JANUARY); // 11 = december
+    	startDateCal.set(Calendar.DAY_OF_MONTH, 1); // new years eve
+    	
+    	Calendar endDateCal = new GregorianCalendar();
+    	endDateCal.set(Calendar.YEAR, 2014);
+    	endDateCal.set(Calendar.MONTH, 11); // 11 = december
+    	endDateCal.set(Calendar.DAY_OF_MONTH, 1);
+    	
+	//	Course course = new Course("COMP666-14D", startDateCal, endDateCal);
+		
+		Time time11 = Time.valueOf("11:00:00");
+		Time time02 = Time.valueOf("11:30:00");
+		Time time1530 = Time.valueOf("15:30:00");
+		Time time1430 = Time.valueOf("14:30:00");
+//		
+//		Event lab = new Event(Calendar.MONDAY, "LAB", time11, 3, "GB.1.666", "Waikato");
+//		Event lec = new Event(Calendar.FRIDAY, "LEC", time02, 1, "GB.1.666", "Waikato");
+//		Event lec1 = new Event(Calendar.TUESDAY, "LAB", time1530, 1, "GB.1.666", "Waikato");
+//		Event tut = new Event(Calendar.WEDNESDAY, "LAB", time1430, 1, "GB.1.666", "Waikato");
+//		
+//		course.addEvents(lab);
+//		course.addEvents(lec);
+//		course.addEvents(lec1);
+//		course.addEvents(tut);
+		
+		TimeTableController con = new TimeTableController();
+			
+//		con.addCourse(course, getContentResolver(), this);
+	}
 
-
-    @Override
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -32,5 +92,36 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public void addCalendarEvent(){   	
+    	long calID = 1;
+    	long startMillis = 0; 
+    	long endMillis = 0;     
+    	Calendar beginTime = Calendar.getInstance();
+    	beginTime.set(2014, 9, 4, 7, 30);
+    	startMillis = beginTime.getTimeInMillis();
+    	Calendar endTime = Calendar.getInstance();
+    	endTime.set(2014, 9, 4, 8, 45);
+    	endMillis = endTime.getTimeInMillis();
+    	
+
+    	ContentResolver cr = getContentResolver();
+    	ContentValues values = new ContentValues();
+    	values.put(Events.DTSTART, startMillis);
+    	values.put(Events.DTEND, endMillis);
+    	values.put(Events.TITLE, "Phil is the greatest");
+    	values.put(Events.DESCRIPTION, "Meetign with Phil");
+    	values.put(Events.CALENDAR_ID, calID);
+    	values.put(Events.EVENT_TIMEZONE, "America/Los_Angeles");
+    	Uri uri = cr.insert(Events.CONTENT_URI, values);
+
+    	// get the event ID that is the last element in the Uri
+    	float eventID = Long.parseLong(uri.getLastPathSegment());
+
+        new AlertDialog.Builder(this)
+        .setTitle("Event Maybe Added")
+        .setMessage("EventID: " + eventID)
+         .show();
     }
 }
---
+++ Android/src/com/example/grouptest/TimeTableController.java
@@ -0,0 +1,111 @@
+package com.example.grouptest;
+
+import java.sql.Time;
+import java.util.Calendar;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import android.provider.CalendarContract.Events;
+import android.provider.Telephony.Sms.Conversations;
+import android.util.Log;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+/**
+ * 
+ * @author batman
+ * 
+ */
+public class TimeTableController {
+
+	private static final String DEBUG_TAG = "TimeTableController";
+	long eventID = 0;
+
+//	public void addCourse(Course toAdd, ContentResolver cr, Context context) {
+//		Calendar durationStartCal = toAdd.getStartDate();
+//		int durationStartYear = durationStartCal.get(Calendar.YEAR);
+//		int durationStartMonth = durationStartCal.get(Calendar.MONTH);
+//		int durationStartDay = durationStartCal.get(Calendar.DAY_OF_WEEK);
+//
+//		Calendar durationEndCal = toAdd.getEndDate();
+//		int durationEndYear = durationEndCal.get(Calendar.YEAR);
+//		int durationEndMonth = durationEndCal.get(Calendar.MONTH);
+//		int durationEndDay = durationEndCal.get(Calendar.DATE);
+//
+//		// TODO: Make this get the persons CalID
+//		long calID = 2;
+//
+//		for (Event event : toAdd.getEvents()) {
+//			Time eventTime = event.getTime();
+//
+//			String[] time = eventTime.toString().split(":");
+//			int eventStartHour = Integer.parseInt(time[0]);
+//			int eventStartMin = Integer.parseInt(time[1]);
+//
+//			int eventEndHour = eventStartHour + event.getDuration();
+//
+//			String eventDayOfWeek = getDayFromInt(event.getDay());
+//
+//			Calendar beginTime = Calendar.getInstance();
+//			beginTime.set(durationStartYear, durationStartMonth, durationStartDay, eventStartHour, eventStartMin);
+//			long startMillis = beginTime.getTimeInMillis();
+//
+//			Calendar endTime = Calendar.getInstance();
+//			endTime.set(durationStartYear, durationStartMonth, durationStartDay, eventEndHour, eventStartMin);
+//			long endMillis = endTime.getTimeInMillis();
+//
+//			ContentValues values = new ContentValues();
+//			values.put(Events.DTSTART, startMillis);
+//			values.put(Events.DTEND, endMillis);
+//			values.put(Events.TITLE, toAdd.getCourseName() + " " + event.getType());
+//			values.put(Events.DESCRIPTION, toAdd.getCourseName() + " " + event.getType() + " " + event.getRoom());
+//			values.put(Events.CALENDAR_ID, calID);
+//			values.put(Events.EVENT_LOCATION, event.getRoom());
+//			values.put(Events.EVENT_TIMEZONE, "Pacific/Auckland");
+//
+//			values.put(Events.RRULE, "FREQ=WEEKLY;BYDAY=" + eventDayOfWeek + ";UNTIL=" + durationEndYear
+//					+ durationEndMonth + durationEndDay);
+//			Uri uri = cr.insert(Events.CONTENT_URI, values);
+//
+//			// get the event ID that is the last element in the Uri
+//			eventID = Long.parseLong(uri.getLastPathSegment());
+//
+//			// TODO something with eventID
+//
+//			new AlertDialog.Builder(context).setTitle("Event Probably Added").setMessage("EventID: " + eventID).show();
+//		}
+//	}
+
+	private String getDayFromInt(int day) {
+		switch (day) {
+		case 1:
+			return "SU";
+		case 2:
+			return "MO";
+		case 3:
+			return "TU";
+		case 4:
+			return "WE";
+		case 5:
+			return "TH";
+		case 6:
+			return "FR";
+		case 7:
+			return "SA";
+		}
+		return null;
+	}
+
+}
